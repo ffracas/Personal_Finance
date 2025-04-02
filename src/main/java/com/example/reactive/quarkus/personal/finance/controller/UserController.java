f50aa8f8c79304f2b53bb18bb49b3e76f36f3e05
U479687 <fabian.aspeeencina@intesasanpaolo.com>
2025-04-02 17:11:25 +0200

    create user crud operation
    create a user controller, service, repository

--- src/main/java/com/example/reactive/quarkus/personal/finance/controller/UserController.java
+++ src/main/java/com/example/reactive/quarkus/personal/finance/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.reactive.quarkus.personal.finance.controller;
 
+import com.example.reactive.quarkus.personal.finance.model.request.UserRequestDto;
 import com.example.reactive.quarkus.personal.finance.model.response.UserResponseDto;
 import com.example.reactive.quarkus.personal.finance.service.UserService;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
+
+import java.util.Set;
 
 @Path("/user")
 public class UserController {
@@ -20,4 +22,23 @@ public class UserController {
     public Uni<UserResponseDto> getUserById(@PathParam("userId") long userId) {
         return userService.getUserById(userId);
     }
+
+    @GET
+    @Path("/getAllUser")
+    public Multi<Set<UserResponseDto>> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @POST
+    @Path("/createUser")
+    public Uni<UserResponseDto> createUser(UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
+    }
+
+    @PUT
+    @Path("/updateUser")
+    public Uni<UserResponseDto> updateUser(UserRequestDto userRequestDto) {
+        return userService.updateUser(userRequestDto);
+    }
+
 }
--- src/main/java/com/example/reactive/quarkus/personal/finance/repository/UserRepository.java
+++ src/main/java/com/example/reactive/quarkus/personal/finance/repository/UserRepository.java
@@ -5,9 +5,15 @@ import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
     public Uni<User> getUserById(long id) {
         return findById(id);
+    }
+
+    public Uni<List<User>> getAllUser() {
+        return listAll();
     }
 }
